05a8b84b8703ae59df5e99fb8724018f7a35a7e1
KTWIU <kevin-thomas.walter@iu-study.org>
2025-06-30 19:18:20 +0200

    Build läuft, erste Fenster mit QT zum Test erstellt

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include <QWidget>
+
+
+//TEST OB QT FUNKTIONIERT CODE LÖSCHEN
+int main(int argc, char *argv[]){
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Aufgaben-Tracker (Test)");
+    window.resize(400, 200);
+    window.show();
+
+    return app.exec();
+}
